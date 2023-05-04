6ad8ae0046c62161cda38450f0061c051e8bba79
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2023-05-04 15:08:01 -0300

    correção no props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from "./styles";
 function myButton({children, isGrey}) {
     console.log(isGrey)
-    return <Button isBlack={isGrey}>{children}</Button>
+    return <Button isGrey={isGrey}>{children}</Button>
 }
 
 export default myButton
